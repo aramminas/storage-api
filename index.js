32b33b81907c7ef4dd94047a1f8f91ac0c7b4922
Aram <armfiletransfer@gmail.com>
2024-07-19 20:59:04 +0400

    changed versel configs v.3

--- index.js
+++ index.js
@@ -1,40 +1,22 @@
-const cors = require('cors');
-const express = require('express');
-const mongoose = require('mongoose');
-require('dotenv').config();
-
-const routes = require('./src/routes');
-const corsOptions = require('./src/configs/corsOptions.js');
-const welcomeTemplates = require('./src/assets/welcomeTemplates.js');
-const middlewares = require('./src/middlewares');
+import express from 'express'
+import cors from 'cors'
 
 const app = express();
 const port = process.env.PORT || 3000;
-const mongoDbUrl = process.env.MONGO_DB_URL || '';
 
-// middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+
+// Enable CORS for all routes
+app.use(cors());
 
 app.get('/', (req, res) => {
-  res.set('Content-Type', 'text/html');
-  res.send(Buffer.from(welcomeTemplates));
-});
+  res.send('Express server working! storage-api')
+})
 
-app.use(cors(corsOptions));
-app.use('/api', routes);
+app.get("/about", (req, res) => {
+    res.json({"message": "Welcome to the about page storage-api"})
+})
 
-// validation error handling
-app.use(middlewares.notFound);
-app.use(middlewares.errorHandler);
-
-mongoose
-  .connect(mongoDbUrl)
-  .then(() => {
-    console.log('Database Connected!');
-
-    app.listen(port, () => {
-      console.log('The server is running on the port %s', port);
-    });
-  })
-  .catch(() => console.log('Connection failed!'));
+app.listen(port, () => {
+    console.log("Sergeant we have a server on the loose...someone catch it");
+})
